b263f9061967ec5eb029b955cc18ba98ca0ba8f7
Jonatan Frencia <frenciaj@gmail.com>
2016-02-06 09:07:41 -0500

    add category id to products to link tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206132917) do
+ActiveRecord::Schema.define(version: 20160206140337) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "type"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160206132917) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
