4d98c8ca70a5a697cb2489857709bafd95ad1288
Jonatan Frencia <frenciaj@gmail.com>
2016-02-06 09:14:57 -0500

    add condition id to products to link tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206140834) do
+ActiveRecord::Schema.define(version: 20160206141050) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "type"
@@ -30,10 +30,11 @@ ActiveRecord::Schema.define(version: 20160206140834) do
     t.text     "description"
     t.string   "picture"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "category_id"
     t.integer  "user_id"
+    t.integer  "condition_id"
   end
 
   create_table "users", force: :cascade do |t|
