64b5afc1f442e9da64d6414baea8340f6ead00b0
Jonatan Frencia <frenciaj@gmail.com>
2016-02-06 14:32:22 -0500

    fix categories dropdown menu and cents input

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
+    @product.price *= 100 
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
@@ -69,6 +69,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :condition, :picture, :price)
+      params.require(:product).permit(:name, :description, :category_id, :condition, :picture, :price)
     end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true
   has_many :products
+
+
+
+def initialize
+ @cat_name = []
+end
 end
